8a3b9b3214d83fcc024bda9d895bbed225e5fc80
KojoTort/Dante? <150588843+KojoTort@users.noreply.github.com>
2024-07-27 15:16:35 +0300

    Новая версия

--- pyrus.py
+++ pyrus.py
@@ -44,3 +44,61 @@ def иначе_если(cond, then, else_):
         return then()
     else:
         return иначе_функция(else_)
+
+def выбор(a, b):
+	return a or b 
+	
+def и (a, b):
+	return a and b 
+	
+	
+def словарь(**kwargs):
+	return kwargs
+	
+	
+	
+def список(*args):
+	return list(args)
+	
+	
+
+class класс:
+
+	def __init__(self, x, y):
+		self.x = x 
+		self.y = y 
+		
+		
+	def method(self): 
+		return self.x + self.y 
+		
+		
+		
+class структура:
+	def __init__(self, x, y):
+	
+		self.x = x 
+		self.y = y 
+		
+def число(x):
+	return int(x)
+	
+	
+	
+def строка(x):
+	return str(x) 
+	
+	
+def замороженый_сет(*args):
+	return frozenset(args)
+	
+	
+def число_с_точкой(x):
+	return float(x)
+	
+    
+def  ввести(promt= ' '):
+    return input(promt)
+
+
+	
